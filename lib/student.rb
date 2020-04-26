d9b480fdcf2be3928bdbf4505e0e4bee8dae3cda
Nicholas Keller <nic.keller328@gmail.com>
2020-04-26 23:04:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,32 @@ class Student
     student.save
     student
   end
+
+  def self.new_from_db(row)
+    new_student = self.new
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.grade = row[2]
+  new_student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
